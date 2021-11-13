e47e638361a2445a13ee60a458cfc2c5c568afc4
deorth-boffin <deorth@outlook.com>
2021-11-13 23:42:19 +0800

    cleanup

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import sys,os
-from typing import ContextManager
 import click
-from pefile import ExportData
 from updater import Updater
 from utils import JsonConfig
 
